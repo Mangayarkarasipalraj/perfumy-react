ddc9644d270ef2759bdd99f5dadc92c3f4f1f2dc
Mangayarkarasipalraj <mangaimeece@gmail.com>
2026-09-24 22:34:23 +0530

    updated perfumy project

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+function Navbar()
+{
+  return(
+  <nav>
+  <h1>Perfumy</h1>
+  <ul>
+  <li> Home </li>
+  <li> Products </li>
+  <li> About </li>
+  <li> Contact us </li>
+  </ul>
+  </nav>
+  )
+}
+
+export default Navbar
